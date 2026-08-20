55839a2186762e510447b5a568fa6baaba9f4b27
Matimu <mjmabunda0@gmail.com>
2026-08-20 21:36:45 +0200

    feat[auth]: add login endpoint, and createToken helper

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
+using System.Security.Cryptography;
 using ejmabunda_web_api.Models;
 using ejmabunda_web_api.Repositories;
 using ejmabunda_web_api.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<ApiSettings>(builder.Configuration.GetSection("ApiSettings"));
 
 // Only the deployed frontend origin may call this API from a browser.
 const string FrontendCorsPolicy = "FrontendCorsPolicy";
@@ -15,16 +19,40 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(FrontendCorsPolicy, policy =>
     {
-        policy.WithOrigins("https://ejmabunda.dev", "http://localhost:3000")
+        policy.WithOrigins(
+            builder.Configuration.GetSection("ApiSettings")["FrontendUrl"]!.ToString())
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
 });
 
+builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
+using var rsa = RSA.Create(2048);
+var publicKey = new RsaSecurityKey(rsa.ExportParameters(false));
+var secretKey = new RsaSecurityKey(rsa.ExportParameters(true));
+
+builder.Services.AddKeyedSingleton("public", publicKey);
+builder.Services.AddKeyedSingleton("private", secretKey);
+
 // JWT bearer authentication, required by default on controllers (see [Authorize] in
 // ProfileController) and opted out of per-action with [AllowAnonymous] for public reads.
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options => {});
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration.GetSection("ApiSettings")["ApiUrl"]!.ToString(),
+            ValidAudience = builder.Configuration.GetSection("ApiSettings")["ApiUrl"]!.ToString(),
+            IssuerSigningKey = publicKey
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 
@@ -36,6 +64,7 @@ builder.Services.AddDbContext<PortfolioContext>(opt =>
 // Controller -> Service -> Repository per feature; see Repositories/ and Services/.
 builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
 builder.Services.AddScoped<IProfileService, ProfileService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
