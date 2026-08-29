6ce5833738e109773e16c000c06f06918702a3c6
Matimu <mjmabunda0@gmail.com>
2026-08-30 00:22:37 +0200

    fix: update cors policy, and cookie config

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -89,7 +89,7 @@ public class AuthController : ControllerBase
         {
             HttpOnly = true,
             Secure = true,
-            SameSite = SameSiteMode.Strict,
+            SameSite = SameSiteMode.None,
             Expires = DateTime.UtcNow.AddDays(7),
             IsEssential = true
         };
